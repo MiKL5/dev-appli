f9af37386a545e9eb928ab86757848fc20fac2a5
Mi K L <miklgaillard@gmail.com>
2026-07-05 17:32:55 +0200

    feat: add persona_snippet program

--- miniProjects/rust/persona_snippet/src/main.rs
+++ miniProjects/rust/persona_snippet/src/main.rs
@@ -1,10 +1,13 @@
 fn main() {
     let prenom = "Mickael";
-    let age = 31;
-    let money = 1502.2123547869521;
+    let mut age = 30;
+    let money = 1505.9153547869521;
     let couleur_hex = "#00E9A5"; // vert émeraude en hexadécimal
 
     println!(
         "Bonjour 👋,\nJe suis {prenom}, j'ai {age} ans.\nJ'ai {money:.2} € 💶.\nMa couleur préférée est {couleur_hex}, le vert émeraude 💚."
     );
+
+    let age = 31;
+    println!("J'ai maintenant {age} ans.")
 }
